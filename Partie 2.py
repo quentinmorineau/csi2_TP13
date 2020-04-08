c9e8882f03242ebb395b3ea355c8dd49527237df
quentinmorineau <quentin.morineau@isen-ouest.yncrea.fr>
2020-04-08 16:42:47 +0200

    .

--- Partie 2.py
+++ Partie 2.py
@@ -12,6 +12,6 @@ n_root = Node(r.getRoot(),17,5)
 n1_left = Node(n_root.getLeft(),4,6)
 n2_left_left = Node(n1_left.getLeft(),None,3)
 n2_right = Node(n_root.getRight(),19,None)
-n3 = Node(n2_right,21,18)
+n3 = Node(n2_right.getRight(),21,18)
 
 
