ad2617de30cb8a31a25d15d8e2d46b796b6964f5
quentinmorineau <quentin.morineau@isen-ouest.yncrea.fr>
2020-04-08 16:35:42 +0200

    Initial commit

---
+++ Partie 2.py
@@ -0,0 +1,17 @@
+from Partie1 import Node
+
+class BinaryTree :
+    def __init__(self,root):
+        self.__root = root
+
+    def getRoot(self):
+        return self.__root
+
+r = BinaryTree(12)
+n_root = Node(r.getRoot(),17,5)
+n1_left = Node(n_root.getLeft(),4,6)
+n2_left_left = Node(n1_left.getLeft(),0,3)
+n2_right = Node(n_root.getRight(),19,0)
+n3 = Node(n2_right,21,18)
+
+
---
+++ Partie1.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self,val,right,left):
+        self.__v = val
+        self.__r = right
+        self.__l = left
+
+    def getVal(self):
+        return self.__v
+
+    def getRight(self):
+        return self.__r
+
+    def getLeft(self):
+        return self.__l
+
+    def setRight(self,r):
+        self.__r = r
+
+    def setLeft(self,l):
+        self.__l = l
+
+n1 = Node(12,17,5)
+n2 = Node(4,6,8)
